8900ec4955eabef4b5af6cc908c530cc77c4a927
Farhan Jiwani <4298654+farhanjiwani@users.noreply.github.com>
2026-09-08 21:59:58 -0400

    feat: add map and mapb commands

--- main.go
+++ main.go
@@ -1,22 +1,92 @@
 package main
 
 import (
-    "fmt";
     "bufio";
+    "encoding/json";
+    "fmt";
+    "io";
+    "log";
+    "net/http";
     "os";
 )
 
 type config struct {
-    cmdRegistry map[string]cliCommand
+    cmdRegistry	map[string]cliCommand
+    mapNext	string
+    mapPrev	string
+}
+
+type locationAreasResult struct {
+    Count	int		`json:"count"`
+    Next	string		`json:"next"`
+    Previous	string		`json:"previous"`
+    Results	[]locationArea	`json:"results"`
+}
+
+type locationArea struct {
+    Name	string	`json:"name"`
+    Url		string	`json:"url"`
 }
 
 type cliCommand struct {
-    name string
-    description string
-    callback func(*config) error
+    name 	string
+    description	string
+    callback 	func(*config) error
+}
+var commands map[string]cliCommand
+
+func getPokeEndPoint(path string) string {
+    prefix := "https://pokeapi.co/api/v2/"
+    return fmt.Sprintf("%s%s", prefix, path)
 }
 
-var commands map[string]cliCommand
+func commandMapNext(state *config) error {
+    if state.mapNext == "" {
+	fmt.Printf("you're on the last page\n")
+    } else {
+	cmdMap(state.mapNext, state)
+    }
+    return nil
+}
+
+func commandMapPrev(state *config) error {
+    if state.mapPrev == "" {
+	fmt.Printf("you're on the first page\n")
+    } else {
+	cmdMap(state.mapPrev, state)
+    }
+    return nil
+}
+
+func cmdMap(url string, state *config) error {
+    res, err := http.Get(url)
+    if err != nil {
+	log.Fatalf("Error reaching location areas: %v", err)
+    }
+    defer res.Body.Close()
+
+    data, err := io.ReadAll(res.Body)
+    if err != nil || res.StatusCode > 299 {
+	log.Fatalf("Error returned from location areas: %v", err)
+    }
+    //    fmt.Printf("%s\n", data)
+    
+    locAreas := locationAreasResult{}
+    if err := json.Unmarshal(data, &locAreas); err != nil {
+	log.Fatalf("Error parsing location area data: %v", err)
+    }
+
+    state.mapNext = locAreas.Next
+    state.mapPrev = locAreas.Previous
+
+    fmt.Printf("\n")
+    for _, area := range locAreas.Results {
+	fmt.Printf("%s\n", area.Name)
+    }
+    fmt.Printf("\n")
+
+    return nil
+}
 
 func commandExit(*config) error {
     fmt.Println("Closing the Pokedex... Goodbye!")
@@ -39,18 +109,30 @@ func commandHelp(*config) error {
 func main() {
     commands = map[string]cliCommand{
 	"exit": {
-	    name: "exit",
-	    description: "Exit the Pokedex",
-	    callback: commandExit,
+	    name: 		"exit",
+	    description:	"Exit the Pokedex",
+	    callback: 		commandExit,
         },
 	"help": {
-	    name: "help",
-	    description: "Displays a help message",
-	    callback: commandHelp,
+	    name: 		"help",
+	    description:	"Displays a help message",
+	    callback:		commandHelp,
         },
+	"map": {
+	    name:		"map",
+	    description: 	"Displays [first/next] 20 location area names",
+	    callback: 		commandMapNext,
+	},
+	"mapb": {
+	    name:		"mapb",
+	    description: 	"Displays previous 20 location area names",
+	    callback: 		commandMapPrev,
+	},
     }
     stateConfig := config {
-	cmdRegistry: commands,
+	cmdRegistry:	commands,
+	mapNext:	getPokeEndPoint("location-area"),
+	mapPrev:	"",
     }
 
     scanner := bufio.NewScanner(os.Stdin)
